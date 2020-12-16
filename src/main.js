5bdf154491a6cee4f7f4b58147765e287b865258
Narration SD <narrationsd@gmail.com>
2020-12-16 14:44:38 -0800

    Remove vue-json-edit involvement as tangram editor replaces it; also validate that last amend-merge properly went through.

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router';
 import store from './store';
 import CKEditor from '@ckeditor/ckeditor5-vue';
 import './assets/styles/index.css';
-// import JsonEditor from '@hardocs-project/vue-json-edit'
 import JsonEditor from '@hardocs-project/tangram-json-editor'
 import '@hardocs-project/tangram-json-editor/lib/lib/index.esm.css'
 import vueShortkey from 'vue-shortkey'
